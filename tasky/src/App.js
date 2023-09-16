5111ca8a77ca055a47f11dd935cef72f8a8b15e3
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2023-09-16 21:00:32 +0100

    16 09 23 commit before .card fix

---
+++ tasky/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Task from './components/Task';
+
+function App() {
+  const Task = (props) => {
+    
+    return (
+        <div>
+            <p>{props.title}</p>
+            <p>Due: {props.deadline}</p>
+        </div>
+    )
+}
+  return (
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task />
+      <Task title="Dishes" deadline="Today!" />
+      <Task title="Laundry" deadline="Tomorrow">
+        Fold laundry and put away
+    </Task>
+<Task title="Tidy" deadline="Today" />
+    </div>
+  );
+  
+}
+
+export default App;
