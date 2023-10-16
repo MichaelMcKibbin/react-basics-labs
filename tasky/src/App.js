e45f93b153e3c573b9cbde25d3bc9a7868c6de78
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2023-10-16 01:32:12 +0100

    Completed Week 1 Lab

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -3,12 +3,10 @@ import Task from './components/Task';
 
 function App() {
   return (
-    <div className="card">
+    <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" />
-      <Task title="Laundry" deadline="Tomorrow">
-        Fold laundry and put away
-    </Task>
+      <Task title="Dishes" deadline="Today" description="Empty Dishwasher"/>
+      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away"/>
       <Task title="Tidy" deadline="Today" />
     </div>
   );
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -3,10 +3,11 @@ import React from 'react';
 const Task = (props) => {
     
     return (
-        <div>
-            <p>{props.title}</p>
+        <div className="card">
+            <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.children}</p>
+            <p className="description">{props.description}</p>
         </div>
     )
 }
